937376b3d5b6526b6aa106242e5d4a86bfe5619c
Severo-Yannick <severoyannick@gmail.com>
2022-11-04 15:11:51 +0100

    target active link

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,46 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 const Navbar = () => {
   const { user, setUser } = useContext(UserContext);
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
-      <Link href="/blog">Blog</Link>
-      {user && <Link href="/dashboard">Dashboard</Link>}
-      {!user && <Link href="/user/auth">Login</Link>}
+      <Link href="/" className={currentRoute === "/" ? "active" : ""}>
+        Home
+      </Link>
+      <Link href="/about" className={currentRoute === "/about" ? "active" : ""}>
+        About
+      </Link>
+      <Link
+        href="/contact"
+        className={currentRoute === "/contact" ? "active" : ""}
+      >
+        Contact
+      </Link>
+      <Link href="/blog" className={currentRoute === "/blog" ? "active" : ""}>
+        Blog
+      </Link>
+      {user && (
+        <Link
+          href="/dashboard"
+          className={currentRoute === "/dashboard" ? "active" : ""}
+        >
+          Dashboard
+        </Link>
+      )}
+      {!user && (
+        <Link
+          href="/user/auth"
+          className={currentRoute === "/user/auth" ? "active" : ""}
+        >
+          Login
+        </Link>
+      )}
       {user && (
         <Link href="/">
           <button onClick={() => setUser(null)}>
